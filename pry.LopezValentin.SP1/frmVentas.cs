bb5aa54c1a260fb67831764b35dc673e2c9ecaed
valentin Lopez <lopezvalen141@gmail.com>
2022-09-21 19:33:39 -0300

    asdasda

--- pry.LopezValentin.SP1/frmVentas.cs
+++ pry.LopezValentin.SP1/frmVentas.cs
@@ -20,19 +20,45 @@ namespace pry.LopezValentin.SP1
 
         private void cmdCargar_Click(object sender, EventArgs e)
         {
+            bool Bandera = false;
 
-            StreamWriter Ventas = new StreamWriter("./Ventas.txt", true);
-            Ventas.WriteLine(lstTipoFactura.Text + ";" + mskNroFac.Text + ";" + DTPFecha.Text + ";" + mskIDCliente.Text + ";" + mskIdVendedor.Text + ";" + txtMonto.Text);
-            MessageBox.Show("Venta cargada con éxito.");
+            StreamReader Ventas = new StreamReader("./Ventas.txt");
+
+            while (!Ventas.EndOfStream)
+            {
+                string auxVentas = Ventas.ReadLine();
+
+                if (mskNroFac.Text == auxVentas.Substring(2, 7))
+                {
+                    MessageBox.Show("Este codigo ya se encuentra cargado, intente de nuevo");
+                    lstTipoFactura.SelectedIndex = 0;
+                    mskNroFac.Text = "";
+                    DTPFecha.Text = "";
+                    mskIDCliente.Text = "";
+                    mskIdVendedor.Text = "";
+                    txtMonto.Text = "";
+                    lstTipoFactura.Focus();
+                    Bandera = true;
+
+                }
+
+            }
             Ventas.Close();
-            lstTipoFactura.SelectedIndex = 0;
-            mskNroFac.Text = "";
-            DTPFecha.Text = "";
-            mskIDCliente.Text = "";
-            mskIdVendedor.Text = "";
-            txtMonto.Text = "";
-            lstTipoFactura.Focus();
 
+            if (Bandera == false)
+            {
+                StreamWriter srVentas = new StreamWriter("./Ventas.txt", true);
+                srVentas.WriteLine(lstTipoFactura.Text + ";" + mskNroFac.Text + ";" + DTPFecha.Text + ";" + mskIDCliente.Text + ";" + mskIdVendedor.Text + ";" + txtMonto.Text);
+                MessageBox.Show("Ventas cargada con exito");
+                srVentas.Close();
+                lstTipoFactura.SelectedIndex = 0;
+                mskNroFac.Text = "";
+                DTPFecha.Text = "";
+                mskIDCliente.Text = "";
+                mskIdVendedor.Text = "";
+                txtMonto.Text = "";
+                lstTipoFactura.Focus();
+            }
         }
 
     }
